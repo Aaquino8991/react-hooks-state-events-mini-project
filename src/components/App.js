e1d0c0ec0d3e29b7568b4f2298fd894c73821599
Anthony <aquinoanthony851@gmail.com>
2023-09-29 11:57:34 -0600

    State Events Mini Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [categories, setCategories] = useState(CATEGORIES);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const changeCategory = (filteredCategory) => {
+    setSelectedCategory(filteredCategory);
+  };
+
+  const onTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const onDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  // Filter tasks based on the selected category
+  const filteredTasks = selectedCategory === "All"
+    ? tasks
+    : tasks.filter((task) => task.category === selectedCategory);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={categories} changeCategory={changeCategory} selectedCategory={selectedCategory} />
+      <NewTaskForm categories={categories} onTaskFormSubmit={onTaskFormSubmit} />
+      <TaskList tasks={filteredTasks} onDeleteTask={onDeleteTask} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, changeCategory, selectedCategory }) {
+  const handleCategory = (e) => {
+    changeCategory(e.target.value);
+  };
+
+  const displayCategories = categories.map((category) => (
+    <button
+      key={category}
+      value={category}
+      onClick={handleCategory}
+      className={selectedCategory === category ? "selected" : ""}
+    >
+      {category}
+    </button>
+  ));
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {displayCategories}
     </div>
   );
 }
